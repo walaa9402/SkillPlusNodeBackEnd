bf27f1b3a1fa49d86b29ea98fc7c9d0a349fab3e
walaa9402 <walaa9402@gmail.com>
2019-05-29 12:43:26 +0200

    sign up

--- routes/users.js
+++ routes/users.js
@@ -48,9 +48,10 @@ router.post('/login',function(req,res){
 				o.user_phone = result[0].user_phone;
 				o.email = result[0].user_email;
 				o.password = result[0].user_password;
+				o.pic = result[0].user_pic;
 				o.rate = result[0].rates;
 				if(!o.rate){
-					o.rate=-1
+					o.rate=0
 				}
 			res.json({		
 				status : true,
@@ -83,27 +84,44 @@ router.post('/signup',function(req,res){
 				message : err				
 			});			
 		}else{
-			var sql = "SELECT user.* ,(SELECT AVG(value) FROM rate where rate.user_id=user.user_id and user_type='skill') as skill_rate ,(SELECT AVG(value) FROM rate where rate.user_id=user.user_id and user_type='service') as service_rate FROM user where user_email=? and user_password=?";
-			var sqlValues = [email,password];
-			pool.query(sql,sqlValues,function(err,result){
-			var user ={};
-				user.id = result[0].user_id ;
-				user.name = result[0].user_name;
-				user.email = result[0].user_email;
-				user.password = result[0].user_password;
-				user.city = result[0].user_pic;
-				user.skillrate = result[0].skill_rate;
-				user.servicerate = result[0].service_rate;
-
-			res.json({		
-				status : true,
-				userdata : user,
-				message : "done"			
-			});
-			});
+			var sql = "SELECT user.* ,(SELECT AVG((SELECT AVG(value) FROM rate where skill_id=s.skill_id)) FROM skill s where user_id=user.user_id) as rates FROM user where user_id=?";
+			pool.query(sql,[result["insertId"]],function(err,result){
+						if(err){
+					res.json({			
+						status : false,
+						login : {},
+						message : err				
+					});			
+				}else{
+					if (result.length > 0) {
+					var o ={};
+						o.id = result[0].user_id ;
+						o.name = result[0].user_name;
+						o.user_phone = result[0].user_phone;
+						o.email = result[0].user_email;
+						o.password = result[0].user_password;
+						o.rate = result[0].rates;
+						o.pic = result[0].user_pic;
+						if(!o.rate){
+							o.rate=0
+						}
+					res.json({		
+						status : true,
+						userlogined : o,
+						message : "done"			
+					});		
+					}else{
+						res.json({			
+						status : false,
+						userlogined : {},
+						message : "invalid email or password"			
+					});	
+			}
 		}		
 		
 	});
+		}
+	
 });
-
+})
 module.exports = router;
