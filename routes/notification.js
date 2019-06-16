cd16ef1c10dedaf29163d1c129e6a9a7af2f5b30
walaa9402 <walaa9402@gmail.com>
2019-06-16 15:53:16 +0200

    add user id

--- routes/notification.js
+++ routes/notification.js
@@ -7,7 +7,7 @@ var pool = require('../config/config');
 router.post('/',function(req,res){
   var user = req.body.user
   var date = req.body.date
-	var sql = "SELECT DISTINCT u.user_name,u.user_pic,s.skill_name,s.skill_id,(SELECT GROUP_CONCAT(date) FROM skill_schedule where learner_id=u.user_id and skill_id=s.skill_id) as schedule FROM user u INNER JOIN skill_schedule sch ON u.user_id=sch.learner_id INNER JOIN skill s ON sch.skill_id=s.skill_id where sch.last_updated>? and s.user_id=? ORDER BY sch.last_updated";
+	var sql = "SELECT DISTINCT u.user_name,u.user_pic,u.user_id,s.skill_name,s.skill_id,(SELECT GROUP_CONCAT(date) FROM skill_schedule where learner_id=u.user_id and skill_id=s.skill_id) as schedule FROM user u INNER JOIN skill_schedule sch ON u.user_id=sch.learner_id INNER JOIN skill s ON sch.skill_id=s.skill_id where sch.last_updated>? and s.user_id=? ORDER BY sch.last_updated";
 	pool.query(sql,[date,user],function(err,apply){
 				if(err){
 			res.json({			
@@ -45,7 +45,7 @@ router.post('/',function(req,res){
           });			
         } else {
           
-          var sql3="SELECT u.user_name,u.user_pic,n.need_name,f.form_id,(SELECT GROUP_CONCAT(date) FROM need_schedule where form_id=f.form_id) as schedule FROM user u INNER JOIN needs n ON u.user_id=n.user_id INNER JOIN forms f ON n.need_id=f.need_id where f.last_updated>? and flag=1 and f.user_id=?";
+          var sql3="SELECT u.user_name,u.user_pic,u.user_id,n.need_name,f.form_id,(SELECT GROUP_CONCAT(date) FROM need_schedule where form_id=f.form_id) as schedule FROM user u INNER JOIN needs n ON u.user_id=n.user_id INNER JOIN forms f ON n.need_id=f.need_id where f.last_updated>? and flag=1 and f.user_id=?";
           pool.query(sql3,[date,user],function(err,accept){
             if(err){
               res.json({			
